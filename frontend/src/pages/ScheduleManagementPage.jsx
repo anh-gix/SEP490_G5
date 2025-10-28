359b89fcc8694b5eaf833c37dbcbf826b9289489
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-10-28 22:45:20 +0700

    schedule management update

---
+++ frontend/src/pages/ScheduleManagementPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScheduleManagement from '../components/class_management/ScheduleManagement';
+
+const ScheduleManagementPage = () => {
+  return (
+    <div className="schedule-management-page">
+      <ScheduleManagement />
+    </div>
+  );
+};
+
+export default ScheduleManagementPage;

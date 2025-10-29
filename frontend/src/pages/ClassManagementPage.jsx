3034be05627f8ce70a504947cfcba70d7bef5256
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-10-29 21:01:31 +0700

    class management update

---
+++ frontend/src/pages/ClassManagementPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AcademicLayout from '../components/class_management/AcademicLayout';
+import ClassManagement from '../components/class_management/ClassManagement';
+
+const ClassManagementPage = () => {
+  return (
+    <AcademicLayout>
+      <ClassManagement />
+    </AcademicLayout>
+  );
+};
+
+export default ClassManagementPage;

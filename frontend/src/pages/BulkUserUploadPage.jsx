38c25273aac65f8d26d016cbeb1de6a95a1bde63
VietNam-K17HL <Namvvhe172838@fpt.edu.vn>
2025-12-03 19:48:05 +0700

    Update BulkUserUploadPage.jsx

--- frontend/src/pages/BulkUserUploadPage.jsx
+++ frontend/src/pages/BulkUserUploadPage.jsx
@@ -25,7 +25,7 @@ const BulkUserUploadPage = () => {
     const fetchRoles = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('http://localhost:9999/api/roles', {
+        const response = await axios.get('http://localhost:8080/api/roles', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
